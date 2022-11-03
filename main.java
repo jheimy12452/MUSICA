31238ade0fc8da2cf050c5b276aee40890a25b60
jheimy12452 <115821755+jheimy12452@users.noreply.github.com>
2022-11-03 10:42:31 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        guitarra guitarra= new guitarra();
+        saxofon saxofon=new saxofon();
+        piano piano=new piano();
+
+
+
+        piano.sonidoinstrumento();
+        guitarra.sonidoinstrumento();
+         saxofon.sonidoinstrumento();
+
+    }
+
+
+}
